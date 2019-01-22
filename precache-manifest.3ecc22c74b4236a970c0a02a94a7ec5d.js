fdef06c250662055ef3a78b8ae693da4af1c2bc5
Alan Daniel <adaniel@grupoasesores.com.mx>
2019-01-22 12:44:52 -0600

    add project

---
+++ precache-manifest.3ecc22c74b4236a970c0a02a94a7ec5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "383070a7c007a42e86bf",
+    "url": "/static/js/main.383070a7.chunk.js"
+  },
+  {
+    "revision": "cdc567f1add3d75916d1",
+    "url": "/static/js/1.cdc567f1.chunk.js"
+  },
+  {
+    "revision": "383070a7c007a42e86bf",
+    "url": "/static/css/main.bbfaea0d.chunk.css"
+  },
+  {
+    "revision": "127b4057b196cb43d3a2f4bc1fbfb04d",
+    "url": "/index.html"
+  }
+];
